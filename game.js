96ce11e622701a0c4cf817a798eedadd40bf3a99
Philippe Lanougadere <soufiane92230@gmail.com>
2020-11-19 22:41:59 +0100

    Add rounds logic and display them

--- game.js
+++ game.js
@@ -1,8 +1,14 @@
-
+// Create variables
 const choices = ['rock', 'paper', 'scissors'];
+
 let userChoice = '';
 let computerChoice = '';
-let user = ''
+
+let userCount = 0;
+let computerCount = 0; 
+
+let userRounds = 0;
+let computerRounds = 0;
 
 // Pick a choice randomly 
 const computerPlay = () => {
@@ -18,7 +24,7 @@ const PlayRound = (playerChoice, computerChoice) => {
 
     if (player === 'paper') {
         if (computer === 'paper') {
-            result = 'IT\'S A TIE !' 
+            result = 'IT\'S TIE !' 
         }
         else if (computer === 'rock') {
             result = 'YOU WIN !' 
@@ -26,20 +32,18 @@ const PlayRound = (playerChoice, computerChoice) => {
         else {
             result = 'YOU LOSE !' 
         }    
-    }
-    
+    }    
     else if (player === 'rock') {
         if (computer === 'paper' ) {
             result = 'YOU LOSE !' 
         }
         else if (computer === 'rock') {
-            result = 'IT\'S A TIE !' 
+            result = 'IT\'S TIE !' 
         }
         else {
             result = 'YOU WIN !' 
         }
-    }
-    
+    }    
     else {
         if (computer === 'paper' ) {
             result = 'YOU WIN !' 
@@ -48,31 +52,58 @@ const PlayRound = (playerChoice, computerChoice) => {
             result = 'YOU LOSE !' 
         }
         else {
-            result = 'IT\'S A TIE !' 
+            result = 'IT\'S TIE !' 
         } 
     }   
     return result
 }
 
+
 // Make 5 round games
 const game = (playerChoice) => {
     for (let i=1; i<=5; i++){
-        const computerChoice = computerPlay()
-        console.log(PlayRound(playerChoice, computerChoice)); 
+        roundGame()
     }
 }
 
-document.querySelectorAll('.choice-pics img').forEach(item => {
-    item.addEventListener('click', () => {
-        userChoice = item.alt;
-        computerChoice = computerPlay();
-        document.getElementById('user-choice').src = `img/big-${userChoice}.png`;
-        document.getElementById('computer-choice').src = `img/big-${computerChoice}.png`;
-        let resultToDisplay = PlayRound(userChoice, computerChoice);
-        document.getElementById('result').innerHTML = resultToDisplay
-    })
-})
 
-// game();
+// Handle players clicks
+    document.querySelectorAll('.choice-pics img').forEach(item => {
+        item.addEventListener('click', () => {
+            
+            userChoice = item.alt;
+            computerChoice = computerPlay();
+            
+            // Update match pics 
+            document.getElementById('user-choice').src = `img/big-${userChoice}.png`;
+            document.getElementById('computer-choice').src = `img/big-${computerChoice}.png`;
+            
+            // Update result text 
+            let resultToDisplay = PlayRound(userChoice, computerChoice);
+            document.getElementById('result').innerHTML = resultToDisplay;
+            
+            // Update players counts
+            result === 'YOU WIN !' && userCount++ 
+            result === 'YOU LOSE !' && computerCount++;
+            document.getElementById('userCount').innerHTML = userCount;
+            document.getElementById('computerCount').innerHTML = computerCount;
+            
+            // Update players rounds
+            
+            if (userCount === 5 || computerCount === 5) {  
+                userCount === 5 ? userRounds++ : computerRounds++;              
+                document.getElementById('userCount').innerHTML = 'Round: ' + userRounds;
+                document.getElementById('computerCount').innerHTML = 'Round: ' + computerRounds;
+                userCount = 0;
+                computerCount = 0 
+            } 
+
+
+
+        })
+    })
+
+
+
 
 
