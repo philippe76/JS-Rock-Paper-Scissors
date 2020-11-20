5c78d2bd0263bf5611797cf4a5da2271a0b169cb
Philippe Lanougadere <soufiane92230@gmail.com>
2020-11-20 12:57:25 +0100

    Improve final victory logic

--- game.js
+++ game.js
@@ -10,11 +10,16 @@ let computerCount = 0;
 let userRounds = 0;
 let computerRounds = 0;
 
+let endRound;
+let setData;
+
+
 // Pick a choice randomly 
 const computerPlay = () => {
     const randomIndex = Math.floor(Math.random()*choices.length);
     return choices[randomIndex]
 }
+
 
 // Make a round game
 const PlayRound = (playerChoice, computerChoice) => {
@@ -60,11 +65,11 @@ const PlayRound = (playerChoice, computerChoice) => {
 
 
 // Make 5 round games
-const game = (playerChoice) => {
-    for (let i=1; i<=5; i++){
-        roundGame()
-    }
-}
+// const game = (playerChoice) => {
+//     for (let i=1; i<=5; i++){
+//         roundGame()
+//     }
+// }
 
 
 // Handle players clicks
@@ -89,47 +94,92 @@ document.querySelectorAll('.choice-pics img').forEach(item => {
         document.getElementById('computerCount').innerHTML = computerCount;
         
         // Handle round end            
-        if (userCount === 5 || computerCount === 5) {    
-            userCount === 5 ? userRounds++ : computerRounds++;     
-            var endRound = setTimeout( () => {
-                document.getElementById('userCount').innerHTML = 'Rounds: ' + userRounds;
-                document.getElementById('computerCount').innerHTML = 'Rounds: ' + computerRounds;
-                document.getElementById('result').innerHTML = 'NEW ROUND';
-                document.getElementById('user-choice').src = `img/transparent.png`;
-                document.getElementById('computer-choice').src = `img/transparent.png`;
-            }, 1000);  
+        if (userCount === 2 || computerCount === 2) {    
+
+            winRound();
+            // userCount === 5 ? userRounds++ : computerRounds++;     
+            // var endRound = setTimeout( () => {
+            //     document.getElementById('userCount').innerHTML = 'Rounds: ' + userRounds;
+            //     document.getElementById('computerCount').innerHTML = 'Rounds: ' + computerRounds;
+            //     document.getElementById('result').innerHTML = 'NEW ROUND';
+            //     document.getElementById('user-choice').src = `img/transparent.png`;
+            //     document.getElementById('computer-choice').src = `img/transparent.png`;
+            // }, 1000);  
             
-            userCount = 0;
-            computerCount = 0 
+            // userCount = 0;
+            // computerCount = 0 
         
-            setTimeout( () => { 
-                document.getElementById('userCount').innerHTML = userCount;
-                document.getElementById('computerCount').innerHTML = computerCount;
-                document.getElementById('userRounds').innerHTML = userRounds;
-                document.getElementById('computerRounds').innerHTML = computerRounds;
-                }, 3000);     
+            // setTimeout( () => { 
+            //     document.getElementById('userCount').innerHTML = userCount;
+            //     document.getElementById('computerCount').innerHTML = computerCount;
+            //     document.getElementById('userRounds').innerHTML = userRounds;
+            //     document.getElementById('computerRounds').innerHTML = computerRounds;
+            //     }, 3000);     
         }       
         
         // Handle game end 
-        if (userRounds === 2 || computerRounds === 2) {            
-            document.getElementById('userRounds').innerHTML = null;
-            document.getElementById('computerRounds').innerHTML = null;
-            userCount = 0;
-            computerCount = 0;
-            document.getElementById('result').innerHTML = userRounds === 2 ? 'YOU WIN !' : 'YOU LOSE !';
-            document.getElementById('userCount').innerHTML = 'Rounds: ' + userRounds;
-            document.getElementById('computerCount').innerHTML = 'Rounds: ' + computerRounds;
-            document.getElementById('user-choice').src = `img/transparent.png`;
-            document.getElementById('computer-choice').src = `img/transparent.png`;
-            clearTimeout(endRound)
+        if (userRounds === 2 || computerRounds === 2) {  
+            
+            winGame();
+            // document.getElementById('userRounds').innerHTML = null;
+            // document.getElementById('computerRounds').innerHTML = null;
+            // userCount = 0;
+            // computerCount = 0;
+            // document.getElementById('result').innerHTML = userRounds === 2 ? 'YOU WIN !' : 'YOU LOSE !';
+            // document.getElementById('userCount').innerHTML = 'Rounds: ' + userRounds;
+            // document.getElementById('computerCount').innerHTML = 'Rounds: ' + computerRounds;
+            // document.getElementById('user-choice').src = `img/transparent.png`;
+            // document.getElementById('computer-choice').src = `img/transparent.png`;
+            // clearTimeout(endRound)
         }
 
     })
 })
 
-    
 
+function winRound() {
+
+    userCount === 2 ? userRounds++ : computerRounds++;   
+
+    endRound = setTimeout( () => {
+        document.getElementById('userCount').innerHTML = 'Rounds: ' + userRounds;
+        document.getElementById('computerCount').innerHTML = 'Rounds: ' + computerRounds;
+        document.getElementById('result').innerHTML = 'NEW ROUND';
+        document.getElementById('user-choice').src = `img/transparent.png`;
+        document.getElementById('computer-choice').src = `img/transparent.png`;
+    }, 1000);  
+            
+    userCount = 0;
+    computerCount = 0 
+        
+    setData = setTimeout( () => { 
+        document.getElementById('userCount').innerHTML = userCount;
+        document.getElementById('computerCount').innerHTML = computerCount;
+        document.getElementById('userRounds').innerHTML = userRounds;
+        document.getElementById('computerRounds').innerHTML = computerRounds;
+    }, 3000);     
+}
+
+
+function winGame() {
+    document.getElementById('userRounds').innerHTML = null;
+    document.getElementById('computerRounds').innerHTML = null;
+    userCount = 0;
+    computerCount = 0;
+    document.getElementById('result').innerHTML = userRounds === 2 ? 'WIN GAME!' : 'LOSE GAME!';
+    document.getElementById('userCount').innerHTML = "SCORE: " + userRounds;
+    document.getElementById('computerCount').innerHTML = "SCORE: " + computerRounds;
+    document.getElementById('user-choice').src = `img/transparent.png`;
+    document.getElementById('computer-choice').src = `img/transparent.png`;
+    clearTimeout(endRound);
+    clearTimeout(setData);
+}
+                
+
+                
+// Get dynamic date for Footer
 document.getElementById('date').innerHTML = new Date().getFullYear()
-
-
-
+                
+                
+                
+                
